720db5fef262b2d7e4bee20e05e43801b041ffb5
Roman Lickevich <r.lickevich@gmail.com>
2018-11-10 17:49:57 +0200

    section team acc

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,17 @@
-import gulp from "gulp";
-import del from "del";
-import ghPages from "gulp-gh-pages";
-import sass from "gulp-sass";
-import postcss from "gulp-postcss";
-import autoprefixer from "autoprefixer";
-import plumber from "gulp-plumber";
-import browserSync from "browser-sync";
+import gulp from 'gulp';
+import del from 'del';
+import ghPages from 'gulp-gh-pages';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import pxtorem from 'postcss-pxtorem';
+
+import svgsprite from 'gulp-svg-sprite';
+import svgmin from 'gulp-svgmin';
+import cheerio from 'gulp-cheerio';
+
+import plumber from 'gulp-plumber';
+import browserSync from 'browser-sync';
 
 //browser-sync
 const server = browserSync.create();
@@ -18,7 +24,7 @@ function reload(done) {
 function serve(done) {
   server.init({
     server: {
-      baseDir: "./dist"
+      baseDir: './dist'
     }
   });
   done();
@@ -27,40 +33,49 @@ function serve(done) {
 //Пути
 const paths = {
   styles: {
-    src: "src/styles/scss/main.scss",
-    watch: "src/styles/scss/**/*.scss",
-    dest: "dist/styles/"
+    src: 'src/styles/scss/main.scss',
+    watch: 'src/styles/scss/**/*.scss',
+    dest: 'dist/styles/'
   },
   stylesNlz: {
-    src: "node_modules/normalize.css/normalize.css",
-    dest: "dist/styles/found"
+    src: 'node_modules/normalize.css/normalize.css',
+    dest: 'dist/styles/found'
   },
   scripts: {
-    src: "src/scripts/main.js",
-    dest: "dist/scripts/"
+    src: 'src/scripts/main.js',
+    dest: 'dist/scripts/'
   },
   html: {
-    src: "src/*.html",
-    dest: "dist/"
+    src: 'src/*.html',
+    dest: 'dist/'
   },
   images: {
-    src: "src/images/**/*.*",
-    dest: "dist/images/"
+    src: 'src/images/**/*.*',
+    dest: 'dist/images/'
   },
   fonts: {
-    src: "src/fonts/**/*.*",
-    dest: "dist/fonts/"
+    src: 'src/fonts/**/*.*',
+    dest: 'dist/fonts/'
+  },
+  svgSprite: {
+    src: 'src/sprite/*.svg',
+    dest: 'dist/images/'
   }
 };
 
 //Удаление папки dist
-const clean = () => del(["dist"]);
+const clean = () => del(['dist']);
 
 //Стили
 function styles() {
   let plugins = [
     autoprefixer({
-      browsers: ["last 10 version", "IE 11", "Firefox ESR"]
+      browsers: ['last 10 version', 'IE 11', 'Firefox ESR']
+    }),
+    pxtorem({
+      propList: ['*'],
+      selectorBlackList: [':root'],
+      minPixelValue: 7
     })
   ];
   return gulp
@@ -98,6 +113,38 @@ function scripts() {
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
+function svgSprite() {
+  return gulp
+    .src(paths.svgSprite.src)
+    .pipe(
+      svgmin({
+        js2svg: {
+          pretty: true
+        }
+      })
+    )
+    .pipe(
+      cheerio({
+        run: $ => {
+          $('[fill]').removeAttr('fill');
+          $('[stroke]').removeAttr('stroke');
+          $('[style]').removeAttr('style');
+        },
+        parserOptions: { xmlMode: true }
+      })
+    )
+    .pipe(
+      svgsprite({
+        mode: {
+          symbol: {
+            sprite: 'sprite.svg'
+          }
+        }
+      })
+    )
+    .pipe(gulp.dest(paths.svgSprite.dest));
+}
+
 //Наблюдение за изменениями
 function watch() {
   gulp.watch(paths.html.src, gulp.series(html, reload));
@@ -105,28 +152,29 @@ function watch() {
   gulp.watch(paths.styles.watch, gulp.series(styles, reload));
   gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
   gulp.watch(paths.images.src, gulp.series(images, reload));
+  gulp.watch(paths.svgSprite.src, gulp.series(svgSprite, reload));
 }
 
 //Создание ветки gh-pages и загрузка туда содержимого папки dist
-gulp.task("deploy", () => {
-  return gulp.src("./dist/**/*").pipe(ghPages());
+gulp.task('deploy', () => {
+  return gulp.src('./dist/**/*').pipe(ghPages());
 });
 
 //Запуск сборки
 gulp.task(
-  "build",
+  'build',
   gulp.series(
     clean,
-    gulp.parallel(html, styles, scripts, stylesNlz, fonts, images)
+    gulp.parallel(html, styles, scripts, stylesNlz, fonts, images, svgSprite)
   )
 );
 
 //Запуск сборки, сервера и слежения за файлами
 gulp.task(
-  "default",
+  'default',
   gulp.series(
     clean,
-    gulp.parallel(html, styles, stylesNlz, scripts, fonts, images),
+    gulp.parallel(html, styles, stylesNlz, scripts, fonts, images, svgSprite),
     gulp.parallel(serve, watch)
   )
 );
