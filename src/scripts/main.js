ea1c12f083c73d9c6d5f36240ad2507bea89b972
Roman Lickevich <r.lickevich@gmail.com>
2018-11-22 19:32:39 +0200

    tz3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,8 @@ const navTrigger = document.querySelector('.nav-trigger');
 navTrigger.addEventListener('click', function(e) {
           e.preventDefault();
           const navMobile = document.querySelector('.nav-mobile');
-          navMobile.classList.toggle('nav-mobile--active');        
+          navMobile.classList.toggle('nav-mobile--active');
+          navTrigger.classList.toggle('nav-trigger--active');        
 });
 
 // const menuItem = document.querySelector('menu_item');
