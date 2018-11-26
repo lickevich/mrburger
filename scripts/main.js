dd028c5272cb4052c0976ba6d26888f7eaa56b8f
Roman Lickevich <r.lickevich@gmail.com>
2018-11-27 00:08:31 +0200

    Update 2018-11-26T22:08:24.267Z

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@ function mobileMenu() {
 
     const navTrigger = document.querySelector('.nav-trigger');
     const navMobile = document.querySelector('.nav-mobile');
-    const navMobileItem = document.querySelectorAll('.nav-mobile__item');
+    const navMobileItems = document.querySelectorAll('.nav-mobile__item');
 
     navTrigger.addEventListener('click', toggleClass);
      
-    for (const iterator of navMobileItem) {
+    for (const iterator of navMobileItems) {
         iterator.addEventListener('click', toggleClass)
     }
 
@@ -19,4 +19,97 @@ function mobileMenu() {
         navTrigger.classList.toggle('nav-trigger--active');
     }
 }
+
 mobileMenu();
+
+// АККОРДЕОН МЕНЮ
+
+function accordeonMenu() {
+    const menuItems = document.querySelectorAll('.menu__item');
+    const menuAccord = document.querySelector('.menu__acco');
+
+    menuAccord.addEventListener('click', event => {
+        event.preventDefault();
+        let target = event.target.parentNode;
+        let content = target.nextElementSibling;
+        let item = target.parentNode;
+
+        const tarWidth = target.clientWidth;
+        const windowWidth = document.documentElement.clientWidth;
+        const layoutContentWidth = 520;
+        const breackpointPhone = 480;
+        const closeMenuWidth = tarWidth * menuItems.lenght;
+        const openMenuWidth = closeMenuWidth + layoutContentWidth;
+
+        if (event.target.classList.contains('menu__title')) {
+          moveMenu();
+        }
+
+        target = event.target;
+        content = target.nextElementSibling;
+        item = target.parentNode;
+
+        if (target.classList.contains('menu__link')) {
+            moveMenu();
+        }
+
+        function moveMenu() {
+            for (const iterator of menuItems) {
+                if (iterator != item) {
+                    iterator.classList.remove('menu__item--active');
+                    iterator.lastElementChild.style.width = 0;
+                    menuAccord.style.transform = `translateX(0)`;
+                }
+            }
+
+            if (item.classList.contains('menu__item--active')) {
+                item.classList.remove('menu__item--active');
+                content.style.width = 0;
+            } else {
+                item.classList.add('menu__item--active');
+                    
+                if (windowWidth > breackpointPhone && windowWidth < openMenuWidth) {
+                    content.style.width = windowWidth - closeMenuWidth + 'px';
+                } else if (windowWidth <= breackpointPhone) {
+                    let num;
+                    for (let i = 0; i < menuItems.length; i++) {
+                        if (menuItems[i] === item) {
+                            num = menuItems.length - (i + 1);
+                        }
+                        menuAccord.style.transform = `translateX(${tarWidth * num}px)`;
+                        content.style.width = windowWidth - tarWidth + 'px';
+                    }
+                } else {
+                    content.style.width = layoutContentWidth + 'px';
+                }
+            }
+        }
+    });
+}
+
+accordeonMenu();
+
+// АККОРДЕОН КОМАНДА
+    const accordElements = document.querySelector('.team__acco');
+    createAccord(accordElements);
+    
+    function createAccord(element) {
+        
+    let activeCont = document.querySelector('.team__acco-content--active');
+
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        
+        const trigger = event.target;
+
+        if (trigger.classList.contains('team__acco-trigger')) {
+            
+            if (activeCont) {
+                activeCont.classList.remove('team__acco-content--active');
+            }
+
+            activeCont = trigger.nextElementSibling;
+            activeCont.classList.add('team__acco-content--active');
+        }
+    });
+}
