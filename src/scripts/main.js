2f5cd14fddf986edd8207cfed3c99ef87f9d0109
Roman Lickevich <r.lickevich@gmail.com>
2018-11-29 12:35:07 +0200

    слайдер бургера готов

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -125,7 +125,7 @@ function sliderBurger() {
     const next = document.querySelector('.scroll-btn--next');
     const prev = document.querySelector('.scroll-btn--prev');
     const list = document.querySelector('.slider__list');
-    const items = list.querySelectorAll('slider__item');
+    const items = list.querySelectorAll('.slider__item');
 
     next.addEventListener('click', moveNext);
     prev.addEventListener('click', movePrev);
@@ -133,10 +133,38 @@ function sliderBurger() {
     let num = 2;
 
     function moveNext() {
-        num++
-        if (num > items.lenght) {
+        num++;
+        if (num > items.length) {
             num = 1;
         }
+        setOrder()
+        list.classList.remove('is-reverse')
+        moveItem();
+    }
+    function movePrev() {
+        num--;
+        if (num === 0) {
+            num = items.length;
+        }
+        setOrder()
+        list.classList.add('is-reverse')
+        moveItem();
+    }
+    function setOrder() {
+        let key = num;
+        for (const i of items) {
+            i.style.order = key;
+            key++
+            if (key > items.length) {
+                key = 1;
+            }
+        }
+    }
+    function moveItem() {
+        list.classList.remove('is-move');
+        setTimeout(() => {
+            list.classList.add('is-move');
+        }, 50);
     }
 }
 sliderBurger();
