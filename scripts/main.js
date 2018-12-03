a9e959142508145c3ab6f60e52a65761450a3ec3
Roman Lickevich <r.lickevich@gmail.com>
2018-12-03 23:38:31 +0200

    Update 2018-12-03T21:38:21.486Z

--- scripts/main.js
+++ scripts/main.js
@@ -442,51 +442,109 @@ function onePageScroll() {
 onePageScroll();
 
 //Плеер YouTube
-function youtubePlayer() {
-
-    let player;
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('yt-player', {
-            height: '405',
-            width: '660',
-            videoId: 'M7lc1UVf-VE',
-            playerVars: {
-                controls: 0,
-                disablekb: 0,
-                modestbranding: 0,
-                rel: 0,
-                autoplay: 0,
-                showinfo: 0
-            },
-            events: {
-            // 'onReady': onPlayerReady,
-            // 'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-
-    $('.player__start').on('click', e => {
-
-        // Возвращает состояние проигрывателя. Возможные значения:
-        // -1 – воспроизведение видео не началось
-        // 0 – воспроизведение видео завершено
-        // 1 – воспроизведение
-        // 2 – пауза
-        // 3 – буферизация
-        // 5 – видео находится в очереди
-
-        const playerStatus = player.getPlayerState();
-
-        if (playerStatus !== 1 ) {
-            player.playVideo();
-        } else {
-            player.pauseVideo();
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('yt-player', {
+        height: '405',
+        width: '660',
+        videoId: 'M7lc1UVf-VE',
+        playerVars: {
+            controls: 0,
+            disablekb: 0,
+            modestbranding: 0,
+            rel: 0,
+            autoplay: 0,
+            showinfo: 0
+        },
+        events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
         }
-
-        player.playVideo()
     });
 }
-youtubePlayer();
+
+function onPlayerStateChange(event) {
+
+    switch(event.data) {
+        case 1:
+            $('.player__start').addClass('paused');
+            $('.player__wrapper').addClass('active');
+            break;
+        case 2:
+            $('.player__start').removeClass('paused');
+    }
+}
+
+function onPlayerReady() {
+    const duration = player.getDuration();
+    let interval;
+
+    $('.player').removeClass('hidden');
+    updateTimer();
+    clearInterval(interval);
+    interval = setInterval(() => {
+        const completed = player.getCurrentTime();
+        const percent = (completed / duration) * 100;
+
+        updateTimer();
+        changeButtonPosition(percent);
+
+    }, 1000);
+}
+
+function updateTimer() {
+    $('.player__duration-completed').text(formatTime(player.getCurrentTime()));
+    $('.player__duration-estimate').text(formatTime(player.getDuration()));
+}
+
+function formatTime(time) {
+    const roundTime = Math.round(time);
+
+    const minutes = Math.floor(roundTime / 60);
+    const seconds = roundTime - minutes * 60;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return minutes + ":" + formattedSeconds;
+}
+
+$('.player__start').on('click', e => {
+
+    // Возвращает состояние проигрывателя. Возможные значения:
+    // -1 – воспроизведение видео не началось
+    // 0 – воспроизведение видео завершено
+    // 1 – воспроизведение
+    // 2 – пауза
+    // 3 – буферизация
+    // 5 – видео находится в очереди
+
+    const playerStatus = player.getPlayerState();
+
+    if (playerStatus !== 1 ) {
+        player.playVideo();
+    } else {
+        player.pauseVideo();
+    }
+});
+
+$('.player__playback').on('click', e => {
+    const bar = $(e.currentTarget);
+    const newButtonPosition = e.pageX - bar.offset().left;
+    const clickedPercent = (newButtonPosition / bar.width()) * 100;
+    const newPlayerTime = (player.getDuration() / 100) * clickedPercent;
+
+    changeButtonPosition(clickedPercent);
+    player.seekTo(newPlayerTime);
+});
+
+$('.player__splash').on('click', e => {
+    player.playVideo();
+})
+
+function changeButtonPosition(percent) {
+    $('.player__playback-button').css({
+        left: `${percent}%`
+    });
+}
 
 //Яндекс карта
 function yandexMap() {
@@ -532,7 +590,7 @@ function yandexMap() {
                 },
                 {
                     iconLayout: 'default#image',
-                    iconImageHref: '../images/svgicons/map-marker.svg',
+                    iconImageHref: './images/svgicons/map-marker.svg',
                     // iconImageClipRect: [[00, 00], [00, 00]], Координаты для изображения из спрайта.
                     iconImageSize: [46, 57],
                     iconImageOffset: [-23, -57] //Сдвиг центра иконки.
@@ -542,7 +600,7 @@ function yandexMap() {
         const clusterer = new ymaps.Clusterer({
             clusterIcons: [
                 {
-                    href: '../images/content/intro/burger_main.png',
+                    href: './images/content/intro/burger_main.png',
                     size: [100, 100],
                     offset: [-50, -50]
                 }
